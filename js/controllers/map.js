e29cd27e96074e7d0cb82134cbb293c0fa4c11f2
Martin Rabaglia <tinchoforever@gmail.com>
2013-06-26 13:02:21 -0300

    another commit to push

--- js/controllers/map.js
+++ js/controllers/map.js
@@ -3,6 +3,20 @@ var initApp = angular.module('initApp.controllers');
 
 initApp.controller('mapController', function ($scope, geolocation, camera, device, localStorageService, points) {
 
+	$scope.mapLocation = function() {
+		$scope.loaderShow = true;
+	   geolocation.getCurrentPosition(function (position) {
+	   	$scope.loaderShow = false;
+	     $scope.position = position;
+	      $scope.map = "http://staticmap.openstreetmap.de/staticmap.php?center=" + position.coords.latitude  + ',' +position.coords.longitude + "&zoom=10&size=400x600&maptype=mapnik&markers="+ position.coords.latitude + ',' +position.coords.longitude +",lightblue1";
+	      points.setLocation(position.coords);
+
+	   });
+
+	   
+	 };
+
+	 $scope.mapLocation();
 
 
 });
