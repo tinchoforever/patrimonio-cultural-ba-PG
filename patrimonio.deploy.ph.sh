e2416d9585acb23c45993e5856ae5b12cf8d51b7
Martin Rabaglia <tinchoforever@gmail.com>
2013-06-26 11:22:30 -0300

    another commit to push

---
+++ patrimonio.deploy.ph.sh
@@ -0,0 +1,8 @@
+cd patrimonio-cultural-ba
+git pull -u
+cd ..
+cp -R patrimonio-cultural-ba/phonegap/core/app/ patrimonio-cultural-ba-PG/
+cd patrimonio-cultural-ba-PG
+git add .
+git commit -m "another commit to push"
+cd ..
