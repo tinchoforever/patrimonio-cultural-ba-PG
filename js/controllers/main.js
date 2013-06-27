a22b8b8f6ec93433478008ed8e7d1c6d48c10c63
Martin Rabaglia <tinchoforever@gmail.com>
2013-06-27 17:06:11 -0300

    another commit to push

--- js/controllers/main.js
+++ js/controllers/main.js
@@ -3,6 +3,8 @@ var initApp = angular.module('initApp.controllers',  ['LocalStorageModule']);
 
 initApp.controller('pointController', function ($scope, geolocation, camera, points) {
 
-
+    if (points.photo){
+        $scope.photo = points.photo;
+    }
 
 });
